2186eed608d1375ecf6d31813b8452dedcd766fe
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2026-02-17 06:50:05 +0100

    Format models

--- internal/model/transaction/transaction.go
+++ internal/model/transaction/transaction.go
@@ -10,12 +10,12 @@ const (
 )
 
 type Transaction struct {
-	ID        string          `json:"id"`
-	Type      TransactionType `json:"type"` // "credit" or "debit"
+	ID          string          `json:"id"`
+	Type        TransactionType `json:"type"` // "credit" or "debit"
 	Description string          `json:"description"`
-	Amount    float64         `json:"amount"`
-	CreatedAt time.Time       `json:"created_at"`
-	CreatedBy string          `json:"created_by"` // Account ID that created the transaction
-	UpdatedAt time.Time       `json:"updated_at"`
-	UpdatedBy string          `json:"updated_by"` // Account ID that last updated the transaction
+	Amount      float64         `json:"amount"`
+	CreatedAt   time.Time       `json:"created_at"`
+	CreatedBy   string          `json:"created_by"` // Account ID that created the transaction
+	UpdatedAt   time.Time       `json:"updated_at"`
+	UpdatedBy   string          `json:"updated_by"` // Account ID that last updated the transaction
 }
